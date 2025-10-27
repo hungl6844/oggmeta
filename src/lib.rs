0c61bdf3ff229bf34c0ab844b9ce5da880ff8189
Akshat Deshpande <akshatd618@gmail.com>
2025-10-27 14:30:32 -0500

    add TryFrom slice for Picture

--- src/lib.rs
+++ src/lib.rs
@@ -250,3 +250,12 @@ impl From<DynamicImage> for Picture {
         }
     }
 }
+
+impl <'a> TryFrom<&'a [u8]> for Picture {
+    type Error = crate::Error;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        let dyn_img = image::load_from_memory(value)?;
+        Ok(dyn_img.into())
+    }
+}
